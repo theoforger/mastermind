0b4b5f4bbebbd912cfe2f7ff9761060920e56e8a
theoforger <theoforger@proton.me>
2024-09-08 17:01:38 -0400

    Add features to read files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::path::PathBuf;
 use clap::Parser;
 
@@ -6,15 +7,37 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 struct Args {
     /// FILE should contain a list of words to link together - the words from your team
-    #[arg(short, long, value_name = "FILE")]
-    link: Option<PathBuf>,
+    #[arg(short, long, required = true, value_name = "FILE")]
+    link: PathBuf,
 
     /// FILE should contain a list of words to avoid - opponent's words, neutral words, and the assassin word
-    #[arg(short, long, value_name = "FILE")]
-    avoid: Option<PathBuf>,
+    #[arg(short, long, required = true, value_name = "FILE")]
+    avoid: PathBuf,
 }
 
 fn main() {
     let args = Args::parse();
-    println!("{:?}", args);
+    
+    let link_words: Vec<String>;
+    let avoid_words: Vec<String>;
+
+    // If the attempt to read both files succeeds, collect each line of the files into their corresponding vector
+    if let (Ok(link_lines), Ok(avoid_lines)) = (fs::read_to_string(args.link), fs::read_to_string(args.avoid)) {
+        link_words = link_lines
+            .lines()
+            .map(|line| line.trim().to_string())
+            .collect();
+        avoid_words = avoid_lines
+            .lines()
+            .map(|line| line.trim().to_string())
+            .collect();
+    } else {
+        eprintln!("Failed to read file(s): Make sure your input path is correct.");
+        std::process::exit(1);
+    }
+
+
+    // Testing
+    eprintln!("Link words: {:?}", link_words);
+    eprintln!("Avoid words: {:?}", avoid_words);
 }
