05c2f2eb4c5ad5d77150f53238ee2c90dbff03df
theoforger <theoforger@proton.me>
2024-09-08 20:11:18 -0400

    Add feature to handle API calls to Groq

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::fs;
+use std::{env, fs};
 use std::path::PathBuf;
 use clap::Parser;
+use dotenv::dotenv;
+use serde_json::json;
+use serde::Deserialize;
 
 /// Mastermind - An LLM-powered CLI tool to help you be a better spymaster in Codenames
 #[derive(Parser)]
@@ -16,6 +19,21 @@ struct Args {
 
     /// Path to a file containing words to avoid - opponent's words, neutral words, and the assassin word
     to_avoid: PathBuf,
+}
+
+#[derive(Deserialize)]
+struct GorqResponse {
+    choices: Vec<Choice>,
+}
+
+#[derive(Deserialize)]
+struct Choice {
+    message: Message,
+}
+
+#[derive(Deserialize)]
+struct Message {
+    content: String,
 }
 
 fn read_words_from_file(path: PathBuf) -> Vec<String> {
@@ -40,7 +58,49 @@ fn generate_prompt(link_words: Vec<String>, avoid_words: Vec<String>) -> String 
     )
 }
 
-fn main() {
+fn build_request_body(prompt: String) -> serde_json::Value {
+    json!({
+        "messages": [
+            {
+                "role": "system",
+                "content": "I am the spymaster in Codenames. I will give you a list of agent words to link together, followed by a list of agent words to avoid. Give me a list of clue words followed by the agent words they are supposed to link together. With each clue word, try to link as many agent words as possible
+                    Here are the requirements:
+                    - Always answer in lower case
+                    - Give five clue word options
+                    - Never give any intro, outro or explanation
+                    - Answer in this format:
+                        [clue] [number of agent words] - [agent word] [agent word] [agent word]
+                    - For the agent words, only give the words themselves. Do not add anything."
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        "model": "llama-3.1-70b-versatile"
+    })
+}
+
+async fn get_answer_from_api_endpoint(endpoint: String, key: String, body: serde_json::Value) -> reqwest::Result<String> {
+    let client = reqwest::Client::new();
+    let response = client
+        .post(endpoint)
+        .bearer_auth(key)
+        .json(&body)
+        .send().await?;
+
+    Ok(response
+        .json::<GorqResponse>()
+        .await?
+        .choices[0]
+        .message
+        .content
+        .to_owned()
+    )
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let link_words = read_words_from_file(args.to_link);
@@ -48,6 +108,14 @@ fn main() {
 
     let prompt = generate_prompt(link_words, avoid_words);
 
-    // Testing
-    println!("{}", prompt);
+    // API
+    dotenv().ok();
+    let api_key = env::var("GROQ_API_KEY")?;
+    let endpoint = env::var("GROQ_API_ENDPOINT")?;
+    
+    let body = build_request_body(prompt);
+    let answer = get_answer_from_api_endpoint(endpoint, api_key, body).await?;
+
+    println!("{}", answer);
+    Ok(())
 }
