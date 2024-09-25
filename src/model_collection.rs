3e5246f9d699adaa516df04ece2810931dc420a5
theoforger <theoforger@proton.me>
2024-09-25 09:33:26 -0400

    Change `get_models()` to return `ModelsResponse`

---
+++ src/model_collection.rs
@@ -0,0 +1,12 @@
+use dialoguer::MultiSelect;
+use crate::json_models::language_models::ModelsResponse;
+
+struct ModelCollection {
+    model_ids: Vec<String>,
+}
+
+impl ModelCollection {
+    pub fn new(response: ModelsResponse) -> Self {
+        
+    }
+}
